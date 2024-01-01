1cde04806b9fa51526eb300424af8cf19acda2b6
sina <sinaderik7@gmail.com>
2024-01-02 00:15:44 +0330

    useRef hook added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { useState, useCallback, useMemo, useEffect } from "react"
+import './App.css'
+import { useState, useCallback, useRef, useMemo, useEffect,ChangeEvent  } from "react"
 
 type FiboFunction = (number: number) => number
-const fiboFunction: FiboFunction=(number: number) => {
+const fiboFunction: FiboFunction = (number: number) => {
   if (number < 2) {
     return number
   }
@@ -9,8 +10,13 @@ const fiboFunction: FiboFunction=(number: number) => {
 }
 export default function App() {
   const [count, setCount] = useState<number>(0)
-  const [users, setUsers] = useState<string[] | null>(['user1', 'user2', 'user3'])
-
+  const [users, setUsers] = useState<string[] | null>(['user1', 'user2', 'user3']);
+  const [inputValue,setInputValue]=useState<string>("")
+  const inputRef = useRef<HTMLInputElement>(null)
+  
+  // console.log(inputRef?.current)
+  // console.log(inputRef?.current?.value)
+  
   useEffect(() => {
     console.log('Mount')
     console.log('users ', users)
@@ -21,17 +27,24 @@ export default function App() {
     setCount(previousNumber => previousNumber + 2)
   }, [])
 
+  const inputHandler=useCallback((e:ChangeEvent<HTMLInputElement>):void=>{
+    setInputValue(e.target.value)
+  },[])
+
   const fixedNumber: number = 37;
   const result: number = useMemo(() => {
     console.log('useMemo')
     return fiboFunction(fixedNumber)
   }, [fixedNumber])
-
+  console.log('component renderd')
   return (
     <div>
       <h2>Count is: {count}</h2>
       <button onClick={addCount}>add</button>
       <h3>{result}</h3>
+      <input ref={inputRef} type="text" /><br /><br />
+      <label>Enter a Text </label>
+      <input onChange={inputHandler} value={inputValue} type="text" />
     </div>
   )
 }
